1d9e3f35af9c8dc91654a5e1c7155a8bdf4ca955
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-13 02:21:29 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,7 +11,7 @@ class MP3Importer
   end
   
   def files
-    Dir.glob("#{@path}/*.mp3").collect {|file| file.gsub("./spec/fixtures/mp3s/")}
+    Dir.glob("#{@path}/*.mp3").include? {|file| file.gsub("./spec/fixtures/mp3s/")}
   end
 
   # Action Bronson - Larry Csonka - indie.mp3
